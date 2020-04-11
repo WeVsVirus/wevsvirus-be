0ca1a7089a4c09d0786885c9295736a972f64837
japana <jp.nachtwey@gmail.com>
2020-04-11 20:25:03 +0200

    removed server certificate SSL is handled by Azure WebApp

--- src/main/java/wirvsvirus/hack/stimmungsbarometer/config/AppConfig.java
+++ src/main/java/wirvsvirus/hack/stimmungsbarometer/config/AppConfig.java
@@ -2,13 +2,8 @@ package wirvsvirus.hack.stimmungsbarometer.config;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-import org.apache.catalina.connector.Connector;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -18,19 +13,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableConfigurationProperties
 public class AppConfig {
 
-    // Logger instance
-    private static final Logger LOG = LoggerFactory.getLogger(AppConfig.class);
-
     @Value("${password}")
     String password;
-
-    @Value("${server.http-port:0}")
-    private Integer serverHttpPort;
-
-    @Bean
-    public WebServerFactoryCustomizer containerCustomizer() {
-        return new TomcatWebServerHttpPortCustomizer();
-    }
 
     /*
      * Use the standard Mongo driver API to create a com.mongodb.MongoClient instance.
@@ -49,22 +33,6 @@ public class AppConfig {
                 registry.addMapping("/**").allowedOrigins("*");
             }
         };
-    }
-
-    public class TomcatWebServerHttpPortCustomizer
-            implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
-        @Override
-        public void customize(TomcatServletWebServerFactory factory) {
-            LOG.info("serverHttpPort property configured as {}", serverHttpPort);
-
-            if (serverHttpPort > 0) {
-                LOG.info("Configuring Http Port to {}", serverHttpPort);
-                Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-                connector.setPort(serverHttpPort);
-                factory.addAdditionalTomcatConnectors(connector);
-            }
-        }
-
     }
 }
 
